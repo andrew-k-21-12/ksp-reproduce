59d656cd4bed8f5618e479650238d81afc040915
Andrey Koshmanskiy <andrew.k.21.12@gmail.com>
2022-08-04 16:16:16 +0300

    Initial - everything works

---
+++ build.gradle.kts
@@ -0,0 +1,27 @@
+plugins {
+    kotlin("js") version "1.7.10"
+}
+
+group = "me.andrey"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react:18.0.0-pre.332-kotlin-1.6.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-react-dom:18.0.0-pre.332-kotlin-1.6.21")
+    implementation("org.jetbrains.kotlin-wrappers:kotlin-emotion:11.9.0-pre.332-kotlin-1.6.21")
+}
+
+kotlin {
+    js(IR) {
+        binaries.executable()
+        browser {
+            commonWebpackConfig {
+                cssSupport.enabled = true
+            }
+        }
+    }
+}
